3048b1fb9924729184de7bcd619e576d031cfb1d
walker <spadonival7@gmail.com>
2024-04-24 19:24:46 -0400

    created a function that returns a license badge based on the license passed in

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,26 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 //THIS IS WHERE I BUILD THE BADGE
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) return "";
+  const badges = {
+    MIT: "license: MIT",
+    GPLv2: "!License: GPL v2",
+    Apache: "!License: Apache",
+    GPLv3: "!License: GPL v3",
+  };
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 //THIS IS WHERE I BUILD UP THE LINK FROM THE LICENSE THAT I CHOSE
 function renderLicenseLink(license) {
-return '[license](https://www.example.com)'
+  return "[license](https://www.example.com)";
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-// THIS IS WHERE I BUILD THE ARE THAT HAS THE LICENSE 
+// THIS IS WHERE I BUILD THE ARE THAT HAS THE LICENSE
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
