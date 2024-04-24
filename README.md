c107e6b14943d56a551afd59c0ee26bcd4503e91
walker <spadonival7@gmail.com>
2024-04-24 15:22:39 -0400

    folder structure

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # ReadMeGenerator
-a command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package.
+! [GitHub Licesnse] (https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+This will generate a readme for all my repositories
+
+## Table of Contents       
+
+* [Installation] (#installation)
+
+* [usage] (#usage)
+
+* [license] (#license)
+
+* [contributing] (#contributing)
+
+* [tests] (#tests)
+
+* [questions] (#questions)
+
+## Installation
+
+To install nescessary dependencies, run the following command:
---
+++ index.js
@@ -0,0 +1,41 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const colors = require("colors");
+const path = require("path");
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "what is your github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "what is your email adress?",
+  },
+  {
+    type: "input",
+    name: "",
+    message: "",
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+  //ask the question
+  inquirer.prompt(questions).then((answers) => {
+    //pass the answer to the big string
+    const string = generateMarkdown(answers);
+    //
+    FileSystem.writefile("readme.md", string);
+  });
+}
+
+// Function call to initialize app
+init();
