c6e0ee2e2b54fcf11ac44767e61f8c7257a23031
walker <spadonival7@gmail.com>
2024-04-24 20:07:47 -0400

    added functions that returns the license link along with function that returns the license section of README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,24 +4,37 @@
 function renderLicenseBadge(license) {
   if (!license) return "";
   const badges = {
-    MIT: "license: MIT",
+    MIT: "License: MIT",
     GPLv2: "!License: GPL v2",
     Apache: "!License: Apache",
     GPLv3: "!License: GPL v3",
   };
+  return badges [license] || '';
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 //THIS IS WHERE I BUILD UP THE LINK FROM THE LICENSE THAT I CHOSE
 function renderLicenseLink(license) {
+  if (!license) return '';
+  const links = {
+    MIT: 'MIT License',
+    GPLv2: 'GPL v2 License',
+    Apache: 'Apache License 2.0',
+    GPLv3: 'GPL v3 License'
+  };
   return "[license](https://www.example.com)";
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-// THIS IS WHERE I BUILD THE ARE THAT HAS THE LICENSE
-function renderLicenseSection(license) {}
+// THIS IS WHERE I BUILD THE AREA THAT HAS THE LICENSE
+function renderLicenseSection(license) {
+  if (!license) return '';
+  return `## License
+This project is licensed under the ${renderLicenseLink(license)}.
+`;  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
