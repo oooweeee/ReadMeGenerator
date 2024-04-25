cfb8a92c49e9905ca3dedcd9b4d470513abcbb34
walker <spadonival7@gmail.com>
2024-04-24 21:37:19 -0400

    created an array of questions for user input

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const colors = require('colors');
-const path = require('path');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const colors = require("colors");
+const path = require("path");
+const inquirer = require("inquirer");
+const { type } = require("os");
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
-    type: "input",
-    name: "github",
-    message: "what is your github username?",
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
   },
   {
-    type: "input",
-    name: "email",
-    message: "what is your email adress?",
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description for your project:',
   },
   {
-    type: "input",
-    name: "",
-    message: "",
+    type: 'input',
+    name: 'installation',
+    message: 'Enter the installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter the usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Enter the contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter the test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'GPLv3', 'Apache', 'BSD', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
   },
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,19 +9,19 @@ function renderLicenseBadge(license) {
     Apache: "!License: Apache",
     GPLv3: "!License: GPL v3",
   };
-  return badges [license] || '';
+  return badges[license] || "";
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 //THIS IS WHERE I BUILD UP THE LINK FROM THE LICENSE THAT I CHOSE
 function renderLicenseLink(license) {
-  if (!license) return '';
+  if (!license) return "";
   const links = {
-    MIT: 'MIT License',
-    GPLv2: 'GPL v2 License',
-    Apache: 'Apache License 2.0',
-    GPLv3: 'GPL v3 License'
+    MIT: "MIT License",
+    GPLv2: "GPL v2 License",
+    Apache: "Apache License 2.0",
+    GPLv3: "GPL v3 License",
   };
   return "[license](https://www.example.com)";
 }
@@ -30,22 +30,31 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 // THIS IS WHERE I BUILD THE AREA THAT HAS THE LICENSE
 function renderLicenseSection(license) {
-  if (!license) return '';
+  if (!license) return "";
   return `## License
 This project is licensed under the ${renderLicenseLink(license)}.
-`;  
+`;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
   ## Description
   ${data.description}
+
+  ## Table of Contents 
   -Installation
   - Usage
   - License
-  ${renderLicenseSection(data.license)}
 
+  ## Installation 
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
 `;
 }
 
